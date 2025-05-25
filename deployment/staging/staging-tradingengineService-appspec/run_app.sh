45da98d8f7aa32bcad911477c1282fb9d1592c7b
Dike Ajah <ekidhaja@gmail.com>
2025-05-26 00:46:29 +0100

    reverted deployment scripts

--- deployment/staging/staging-tradingengineService-appspec/run_app.sh
+++ deployment/staging/staging-tradingengineService-appspec/run_app.sh
@@ -4,13 +4,8 @@ ENV=staging
 
 cd /home/ec2-user/
 
-CONTAINER_NAME=staging-tradingengineService
-
-# Stop and remove the old container if it exists
-if [ "$(sudo docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-    sudo docker stop $CONTAINER_NAME
-    sudo docker rm $CONTAINER_NAME
-fi
+# Stop the running Docker container
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=575439814610.dkr.ecr.eu-west-1.amazonaws.com/staging-tradingengine-service:latest --format="{{.ID}}"))
 
 # Authenticate with AWS ECR
 aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 575439814610.dkr.ecr.eu-west-1.amazonaws.com
@@ -18,10 +13,5 @@ aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS
 # Pull the latest Docker image
 sudo docker pull 575439814610.dkr.ecr.eu-west-1.amazonaws.com/staging-tradingengine-service:latest
 
-# Run the new container with the same name
-sudo docker run -d --name $CONTAINER_NAME -p 80:8080 \
-  -e NODE_ENV=staging -e SERVICE=staging-tradingengineService \
-  --log-driver=awslogs --log-opt awslogs-region=eu-west-1 \
-  --log-opt awslogs-group=/aws/ec2/staging-tradingengineService \
-  --log-opt awslogs-create-group=true \
-  575439814610.dkr.ecr.eu-west-1.amazonaws.com/staging-tradingengine-service:latest
+# Run the newly pulled image
+sudo docker run -d -p 80:8080 -e NODE_ENV=staging -e SERVICE=staging-tradingengineService --log-driver=awslogs --log-opt awslogs-region=eu-west-1 --log-opt awslogs-group=/aws/ec2/staging-tradingengineService --log-opt awslogs-create-group=true 575439814610.dkr.ecr.eu-west-1.amazonaws.com/staging-tradingengine-service:latest
