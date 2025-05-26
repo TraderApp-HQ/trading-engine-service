48dddea33255b46df95ed1cd65fb8a0dbc8cff32
Dike Ajah <ekidhaja@gmail.com>
2025-05-26 02:52:24 +0100

    updated deployment scripts for prod

--- deployment/dev/dev-tradingengineService-appspec/run_app.sh
+++ deployment/dev/dev-tradingengineService-appspec/run_app.sh
@@ -4,8 +4,13 @@ ENV=dev
 
 cd /home/ec2-user/
 
-# Stop the running Docker container
-sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=575439814610.dkr.ecr.eu-west-1.amazonaws.com/dev-tradingengine-service:latest --format="{{.ID}}"))
+CONTAINER_NAME=dev-tradingengineService
+
+# Stop and remove the old container if it exists
+if [ "$(sudo docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    sudo docker stop $CONTAINER_NAME
+    sudo docker rm $CONTAINER_NAME
+fi
 
 # Authenticate with AWS ECR
 aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 575439814610.dkr.ecr.eu-west-1.amazonaws.com
@@ -13,5 +18,10 @@ aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS
 # Pull the latest Docker image
 sudo docker pull 575439814610.dkr.ecr.eu-west-1.amazonaws.com/dev-tradingengine-service:latest
 
-# Run the newly pulled image
-sudo docker run -d -p 80:8080 -e NODE_ENV=development -e SERVICE=dev-tradingengineService --log-driver=awslogs --log-opt awslogs-region=eu-west-1 --log-opt awslogs-group=/aws/ec2/dev-tradingengineService --log-opt awslogs-create-group=true 575439814610.dkr.ecr.eu-west-1.amazonaws.com/dev-tradingengine-service:latest
+# Run the new container with the same name
+sudo docker run -d --name $CONTAINER_NAME -p 80:8080 \
+  -e NODE_ENV=development -e SERVICE=dev-tradingengineService \
+  --log-driver=awslogs --log-opt awslogs-region=eu-west-1 \
+  --log-opt awslogs-group=/aws/ec2/dev-tradingengineService \
+  --log-opt awslogs-create-group=true \
+  575439814610.dkr.ecr.eu-west-1.amazonaws.com/dev-tradingengine-service:latest
--- deployment/prod/prod-tradingengineService-appspec/run_app.sh
+++ deployment/prod/prod-tradingengineService-appspec/run_app.sh
@@ -4,14 +4,24 @@ ENV=prod
 
 cd /home/ec2-user/
 
-# Stop the running Docker container
-sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=575439814610.dkr.ecr.eu-west-1.amazonaws.com/prod-tradingengine-service:latest --format="{{.ID}}"))
+CONTAINER_NAME=prod-tradingengineService
+
+# Stop and remove the old container if it exists
+if [ "$(sudo docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+  prod docker stop $CONTAINER_NAME
+    sudo docker rm $CONTAINER_NAME
+fi
 
 # Authenticate with AWS ECR
 aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 575439814610.dkr.ecr.eu-west-1.amazonaws.com
 
 # Pull the latest Docker image
-sudo docker pull 575439814610.dkr.ecr.eu-west-1.amazonaws.com/prod-tradingengine-service:latest
+sudo docker pull 575439814610.dkr.ecr.eu-west-prodonaws.com/prod-tradingengine-service:latest
 
-# Run the newly pulled image
-sudo docker run -d -p 80:8080 -e NODE_ENV=production -e SERVICE=prod-tradingengineService --log-driver=awslogs --log-opt awslogs-region=eu-west-1 --log-opt awslogs-group=/aws/ec2/prod-tradingengineService --log-opt awslogs-create-group=true 575439814610.dkr.ecr.eu-west-1.amazonaws.com/prod-tradingengine-service:latest
+# Run the new container with the same name
+sudo docker run -d --name $CONTAINER_NAME -p 80:8080 \
+  -e NODE_ENV=production -e SERVICE=prod-tradingengineService \
+  --log-driver=awslogs --log-oprodlogs-region=eu-west-1 \
+  --log-opt awslogs-group=/aws/ec2/prod-tradingengineService \
+  --log-opt awprodcreate-group=true \
+  575439814610.dkr.ecr.eu-west-1.amazonaws.com/prod-tradingengine-service:latest
