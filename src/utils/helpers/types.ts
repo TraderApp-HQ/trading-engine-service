a946bed7b621401f22b85c0f6a16f3538b915e0e
Jacob Oluwasesan Adebayo <jacobadebayo.ja@gmail.com>
2025-07-04 00:25:55 +0100

    [feat: TA-261] User Onboarding Flow (#38)
    * onboarding flow
    
    * Addressed PR comments

---
+++ src/utils/helpers/types.ts
@@ -0,0 +1,15 @@
+export enum UserOnboardingChecklist {
+	IS_EMAIL_VERIFIED = "isEmailVerified",
+	IS_FIRST_DEPOSIT_MADE = "isFirstDepositMade",
+	IS_TRADING_ACCOUNT_CONNECTED = "isTradingAccountConnected",
+	IS_SOCIAL_ACCOUNT_CONNECTED = "isSocialAccountConnected",
+	IS_ONBOARDING_TASK_DONE = "isOnboardingTaskDone",
+	SHOW_ONBOARDING_STEPS = "showOnboardingSteps",
+	IS_PHONE_VERIFIED = "isPhoneVerified",
+	IS_ID_VERIFIED = "isIdVerified",
+}
+
+export interface ITrackUserOnboardingChecklistInput {
+	userId: string;
+	onboardingChecklistItem: UserOnboardingChecklist;
+}
