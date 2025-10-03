55f6af1ca1c472fd850e0d846e112f80b1771806
JacceyCode <jacobadebayo.ja@gmail.com>
2025-10-03 12:01:19 +0100

    reset to default port

--- src/app.ts
+++ src/app.ts
@@ -26,10 +26,10 @@ const secretNames = ["common-secrets", "trading-engine-service-secrets"];
 		secretsJson,
 	});
 
-	// const port = process.env.PORT;
+	const port = process.env.PORT;
 	const dbUrl = process.env.TRADING_ENGINE_SERVICE_DB_URL ?? "";
 
-	const port = 8081;
+	// const port = 8081;
 	// const dbUrl = "mongodb://localhost:27017/trading-service-db";
 	mongoose
 		.connect(dbUrl)
